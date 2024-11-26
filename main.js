60fc0f270a0f2825902157d869e317795606b84b
Ilaria44 <91695386+Ilaria44@users.noreply.github.com>
2024-11-26 18:58:46 +0100

    create rows to get a grid

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ let gridContainer = document.querySelector(".container");
 console.log(gridContainer)
 
 for (i = 0; i < 16; i++) {
-  let newDiv = document.createElement("div");
-  gridContainer.appendChild(newDiv);
+  let rowDiv = document.createElement("div");
+  rowDiv.classList.add("row");
+  
+
+  for (j = 0; j < 16; j++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("square");
+    rowDiv.appendChild(newDiv);
+  }
+
+  gridContainer.appendChild(rowDiv);
+  console.log(i);
+
 }
